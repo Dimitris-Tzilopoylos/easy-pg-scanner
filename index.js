86d26d457c2f8a45dc7246de009faf816b0c90bb
dimtzilop <dimtzilop@iti.gr>
2024-05-25 22:11:01 +0300

    prepares new version

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 const loadDatabase = require("./lib");
 
-loadDatabase({ user: "postgres", port: 5435, password: "postgres" }).then(
-  (x) => {
-    console.log(x);
-  }
-);
-
 module.exports = loadDatabase;
