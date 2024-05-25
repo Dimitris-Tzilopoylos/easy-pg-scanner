ca8e10789f3cb2d89b5b3ab524308abab778013e
dimtzilop <dimtzilop@iti.gr>
2024-05-25 22:09:23 +0300

    removes aggregations from scanned functions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const loadDatabase = require("./lib");
 
+loadDatabase({ user: "postgres", port: 5435, password: "postgres" }).then(
+  (x) => {
+    console.log(x);
+  }
+);
+
 module.exports = loadDatabase;
